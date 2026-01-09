a47a84673f41d3d521d3f75fed85830b3eacd47d
KimSooHwan <2001sonickim@gmail.comm>
2026-01-09 16:03:00 +0900

    node, cmake, package 수정

--- perception/setup.py
+++ perception/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/srv', ['srv/to_estimation.srv']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +25,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'perception_node = perception.nodes.perception_node:main',
         ],
     },
 )

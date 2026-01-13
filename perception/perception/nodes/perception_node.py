481daf6d92599fa0493e2bebb7d022a05de0dcdd
KimSooHwan <2001sonickim@gmail.comm>
2026-01-13 15:00:37 +0900

    numpy, scipy 버전 변경 및 readme에 추가

--- perception/perception/nodes/perception_node.py
+++ perception/perception/nodes/perception_node.py
@@ -1,35 +1,14 @@
-from rost_interfaces.srv import AddTwoInts
+from rost_interfaces.srv import perception_to_estimation
 
 import rclpy
 from rclpy.node import Node
 
 class PerceptionNode(Node):
+
+
     def __init__(self):
         super().__init__('perception_node')
-        self.srv = self.create_service(AddTwoInts, 'add_two_ints', self.add_two_ints_callback)
-        self.srv = self.create_client(AddTwoInts, 'add_two_ints')
-
-        self.get_logger().info('Perception Node has been started and service is ready.')
-
-    def add_two_ints_callback(self, request, response):
-        response.sum = request.a + request.b
-        self.get_logger().info(f'Adding {request.a} + {request.b} = {response.sum}')
-        return response
-    
-    def call_add_two_ints(self, a, b):
+        self.srv = self.create_client(perception_to_estimation, 'perception_to_estimation')
         while not self.srv.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Service not available, waiting again...')
-        
-        request = AddTwoInts.Request()
-        request.a = a
-        request.b = b
-
-        future = self.srv.call_async(request)
-        rclpy.spin_until_future_complete(self, future)
-
-        if future.result() is not None:
-            self.get_logger().info(f'Service call result: {future.result().sum}')
-            return future.result().sum
-        else:
-            self.get_logger().error('Service call failed')
-            return None
+            self.get_logger().info('service not available, waiting again...')
+        self.req = perception_to_estimation.Request()

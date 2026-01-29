2497e59f58e2a74922d02acc4524aca7fdd31e4f
홍준표 <leopardhong1189@gmail.com>
2026-01-29 15:46:41 +0900

    gitignore 수정

--- control/setup.py
+++ control/setup.py
@@ -29,6 +29,7 @@ setup(
             'tower = control.main:main',
             'move = control.moving_test:main',
             'pos = control.pos:main',
+            'grip = control.dev_grip:main',
         ],
     },
 )
